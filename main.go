9999344de0ac1adc3ac785a260b43be361d77904
Vitaliy <vitalikir156@gmail.com>
2024-09-07 03:40:26 +0300

    bad build, only for transferring to another PC

--- main.go
+++ main.go
@@ -1,19 +1,137 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
+	"github.com/jmoiron/sqlx"
 	sqltranslation "github.com/vitalikir156/tasker/dbc"
 	ui "github.com/vitalikir156/tasker/userinterface"
 )
 
-var killer bool
-
 func main() {
+	var task sqltranslation.Tasktable
+	var switcher ui.Switchertype
+	switcher.Allowuserinput = true
 	db := sqltranslation.Start("user=vit password=p!ssword2717 dbname=tasker port=5432 sslmode=disable")
 	defer db.Close()
-	for !killer {
-		killer = ui.UI(db)
+
+	for switcher.Menu0 > -1 {
+		var err error
+
+		out, err := menucore(&switcher, &task, db)
+		if err != nil {
+			fmt.Println(err)
+			err = nil
+		} else {
+			fmt.Println(out)
+		}
+		if switcher.Allowuserinput {
+			switcher.Userinput, err = userinputreader()
+		} else {
+			switcher.Allowuserinput = true
+			switcher.Userinput = ""
+		}
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
 	}
-	fmt.Println("Yeap! Exiting")
+	//var selector
 }
+func menucore(menu *ui.Switchertype, task *sqltranslation.Tasktable, db *sqlx.DB) (string, error) {
+	switch menu.Userinput {
+	case "exit": // Выход из утилиты в любом месте
+		{
+			menu.Allowuserinput = false
+			menu.Menu0 = -1
+			return "exiting", nil
+		}
+	case "main": // Выход в основное меню в любом месте
+		{
+			menu.Userinput = ""
+			task = &sqltranslation.Tasktable{}
+			fmt.Println(task)
+		}
+	}
+	switch menu.Menu0 {
+	case 0:
+		{
+			out, err := ui.Mainmenu(menu)
+			return out, err
+		}
+
+	case 1:
+		{
+			menu.Allowuserinput = false
+			menu.Menu0 = 0
+			return sqltranslation.GetAll(db)
+		}
+	case 2:
+		{
+			out, err := ui.UpdateDate(menu, task)
+
+			return out, err
+		}
+	case 3:
+		{
+			out := "3"
+			return out, nil
+		}
+	case 4:
+		{
+			out := "1"
+			return out, nil
+		}
+	case 5:
+		{
+			out := "1"
+			return out, nil
+		}
+	}
+	return "assss", nil
+}
+
+func userinputreader() (string, error) {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("-> ")
+	text, err := reader.ReadString('\n')
+	text = strings.ReplaceAll(text, "\n", "")
+	// fmt.Println(err)
+	return text, err
+}
+
+/*	if menu.Menu0 == 0 {
+
+		{
+			if len(menu.Userinput) > 0 {
+				x, _ := strconv.Atoi(menu.Userinput)
+				if 0 <= x && x <= 5 {
+					menu.Menu0 = x
+				}
+			} else {
+				a := (`
+-=MAIN MENU=-
+0: exit
+1: get all tasks
+2: create task
+3: edit task
+4: delete task
+select option and hit enter:`)
+				return a, nil
+			}
+		}
+	}
+
+	if menu.Menu0 == 1 {
+		{
+			a := (`
+
+1: get all tasks
+`)
+			return a, nil
+		}
+	}
+	menu.Allowuserinput = false*/
--- userinterface/ui.go
+++ userinterface/ui.go
@@ -11,36 +11,55 @@ import (
 	sqltranslation "github.com/vitalikir156/tasker/dbc"
 )
 
-func insert(db *sqlx.DB) {
-	var task sqltranslation.Tasktable
-	fmt.Printf("enter message:")
-	var err error
-	task.Message, err = userinputreader()
-	if err != nil {
-		fmt.Println(err)
-	}
-	if len(task.Message) < 1 {
-		fmt.Println("No task message entered, exiting")
-	}
+type Switchertype struct {
+	Userinput      string
+	Menu0          int
+	Stage          int
+	Allowuserinput bool
+}
 
-	fmt.Printf("enter status(empty for NEW):")
-	task.Status, err = userinputreader()
-	if err != nil {
-		fmt.Println(err)
+func Post(menu *Switchertype, task *sqltranslation.Tasktable, db *sqlx.DB) (string, error) {
+	var err error
+	switch menu.Stage {
+	case 0:
+		{
+			menu.Stage = +1
+			return "enter task message:", nil
+		}
+	case 1:
+		{
+			if len(menu.Userinput) < 1 {
+				return "Empty message entered, please try again or exit", nil
+			} else {
+				task.Message = menu.Userinput
+				menu.Stage = +1
+				return "enter status(empty for <new>):", nil
+			}
+		}
+	case 2:
+		{
+			if len(menu.Userinput) < 1 {
+				task.Status = "new"
+			} else {
+				task.Status = menu.Userinput
+				menu.Stage = +1
+			}
+			return "enter status(empty for <new>):", nil
+		}
+	case 3:
+		{
+		}
 	}
-	if len(task.Status) < 1 {
-		task.Status = "new"
-	}
-	task.Deadline, err = datetimegetter()
+	task.Deadline, err = Datetimegetter()
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("task without deadline is fiasco, exiting")
-		return
 	}
 	fmt.Printf("status:%v, deadline:%v, message:%v\n", task.Status, task.Deadline, task.Message)
-	res, err := sqltranslation.PostTask(db, task)
+	res, err := sqltranslation.PostTask(db, *task)
 
 	fmt.Println(res, err)
+	return "enter status(empty for <new>):", nil
 }
 
 func update(db *sqlx.DB) {
@@ -115,7 +134,7 @@ func update(db *sqlx.DB) {
 
 		case 4:
 			{
-				date, err2 := datetimegetter()
+				date, err2 := Datetimegetter()
 				if err2 == nil {
 					task.Deadline = date
 				}
@@ -141,55 +160,90 @@ func del(db *sqlx.DB) {
 	fmt.Println(res, err)
 }
 
-func get(db *sqlx.DB) {
-	res, err := sqltranslation.GetAll(db)
-	fmt.Println(res, err)
-}
+/*
+	func UI(db *sqlx.DB) bool {
+		fmt.Print(`
+		-=MAIN MENU=-
 
-func UI(db *sqlx.DB) bool {
-	fmt.Print(`
-	-=MAIN MENU=-
 0: exit
 1: get all tasks
 2: create task
 3: edit task
 4: delete task
-	select option and hit enter:`)
-	var s int
-	_, err := fmt.Scanln(&s)
-	if err != nil {
-		fmt.Println(err)
+
+		select option and hit enter:`)
+		var s int
+		_, err := fmt.Scanln(&s)
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+		switch s {
+		case 0:
+			{
+				return true
+			}
+		case 1:
+			{
+				get(db)
+			}
+		case 2:
+			{
+				insert(db)
+			}
+		case 3:
+			{
+				update(db)
+			}
+		case 4:
+			{
+				del(db)
+			}
+		default:
+			{
+				fmt.Printf("bad input entered: %v\n", s)
+			}
+		}
 		return false
 	}
-	switch s {
-	case 0:
+*/
+func Mainmenu(menu *Switchertype) (string, error) {
+
+	switch menu.Userinput {
+	case "":
 		{
-			return true
+			a := (`
+	-=MAIN MENU=-
+	0: exit
+	1: get all tasks
+	2: create task
+	3: edit task
+	4: delete task
+	select option and hit enter:`)
+			return a, nil
 		}
-	case 1:
+	case "0":
 		{
-			get(db)
+			menu.Menu0 = -1
+			menu.Allowuserinput = false
+			return "exiting", nil
 		}
-	case 2:
+	case "1":
 		{
-			insert(db)
+			menu.Menu0 = 1
+			menu.Allowuserinput = false
+			return "", nil
 		}
-	case 3:
+	case "2":
 		{
-			update(db)
-		}
-	case 4:
-		{
-			del(db)
-		}
-	default:
-		{
-			fmt.Printf("bad input entered: %v\n", s)
+			menu.Menu0 = 2
+			//		menu.Allowuserinput = false
+			return "", nil
 		}
 	}
-	return false
+	menu.Allowuserinput = false
+	return fmt.Sprintf("Something very strange was entered: %v.\nEnter an existing option.", menu.Userinput), nil
 }
-
 func userinputreader() (string, error) {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("-> ")
@@ -198,29 +252,55 @@ func userinputreader() (string, error) {
 	// fmt.Println(err)
 	return text, err
 }
+func UpdateDate(menu *Switchertype, task *sqltranslation.Tasktable) (string, error) {
+	fmt.Println(len(menu.Userinput))
+	if len(menu.Userinput) == 0 {
+		return fmt.Sprintf("entered nothing, keeping date=%v", task.Deadline.Format("02.01.06")), nil
+	}
+	if len(menu.Userinput) != 8 {
+		return "", fmt.Errorf("entered bad sequence: %v, need dd.mm.yy as 02.01.06", menu.Userinput)
 
-func datetimegetter() (time.Time, error) {
-	nowdate := time.Now().Format("02.01.06")
-	nowtime := time.Now().Format("15.04")
-	fmt.Printf("enter deadline date (format dd.mm.yy)(nothing for %v)", nowdate)
+	}
+	now, err := time.Parse("02.01.0615.04", menu.Userinput+task.Deadline.Format("15.04"))
+	if err != nil {
+		return "", err
+	}
+	task.Deadline = now
+	return "date updated without any errors", nil
+}
+
+func Datetimegetter() (time.Time, error) {
+	var err error
+	now := time.Now()
+	fmt.Println(now)
+	//.Format("02.01.06")
+	//	nowtime := time.Now().Format("15.04")
+	fmt.Printf("enter deadline date (format dd.mm.yy)(nothing for %v)", now.Format("02.01.06"))
 	dateread, err2 := userinputreader()
 	if err2 != nil {
 		fmt.Println(err2)
 	}
 	if len(dateread) > 0 {
-		nowdate = dateread
+		now, err = time.Parse("02.01.0615.04", dateread+now.Format("15.04"))
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
-	fmt.Printf("enter deadline time (format hh.mm)(nothing for %v)", nowtime)
+	fmt.Println(now)
+	fmt.Printf("enter deadline time (format hh.mm)(nothing for %v)", now.Format("15.04"))
 	timeread, err2 := userinputreader()
 	if err2 != nil {
 		fmt.Println(err2)
 	}
-	if len(timeread) > 0 {
-		nowtime = timeread
+
+	if len(dateread) > 0 {
+		now, err = time.Parse("02.01.0615.04", now.Format("02.01.06")+timeread)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
-	date, err := time.Parse("02.01.0615.04", nowdate+nowtime)
-	if err != nil {
-		fmt.Println(err)
-	}
-	return date, err
+
+	fmt.Println(now)
+
+	return now, err
 }
